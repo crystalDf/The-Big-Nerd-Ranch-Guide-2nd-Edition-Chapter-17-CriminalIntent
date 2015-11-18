543b9925093c8f5109abfa13a0c1cfd1ab880038
crystalDf <chendong333@gmail.com>
2015-11-18 22:05:34 +0800

    Chapter 17 CriminalIntent

--- app/src/main/java/com/star/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/star/criminalintent/SingleFragmentActivity.java
@@ -2,6 +2,7 @@ package com.star.criminalintent;
 
 
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
@@ -10,10 +11,15 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
 
     protected abstract Fragment createFragment();
 
+    @LayoutRes
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
+        setContentView(getLayoutResId());
 
         FragmentManager fragmentManager = getSupportFragmentManager();
 
