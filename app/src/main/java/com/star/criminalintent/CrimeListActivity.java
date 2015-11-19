ff3741c781e44df50a4894e14aeaf841dbf1e79b
crystalDf <chendong333@gmail.com>
2015-11-19 21:15:36 +0800

    Chapter 17 CriminalIntent

--- app/src/main/java/com/star/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/star/criminalintent/CrimeListActivity.java
@@ -9,4 +9,8 @@ public class CrimeListActivity extends SingleFragmentActivity {
         return new CrimeListFragment();
     }
 
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_twopane;
+    }
 }
